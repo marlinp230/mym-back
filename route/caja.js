868c1ca3edde2fac9d8b77f48528b722ca5f590b
Marlyn <marlinp230@gmail.com>
2022-08-08 16:09:33 -0400

    make it better

--- route/caja.js
+++ route/caja.js
@@ -4,14 +4,14 @@ const Caja1 = require('../models/caja')
 
 router.get('/', async function (req, res, next) {
  
-    // try {
-    //   const cajadb = await Caja1.find()
-    //   res.status(200).json(cajadb)
+    try {
+      const cajadb = await Caja1.find()
+      res.status(200).json(cajadb)
   
-    // } catch (error) {
-    //   res.status(400).json(error)
+    } catch (error) {
+      res.status(400).json(error)
   
-    // }
+    }
     res.send("hello223456")
   
   });
