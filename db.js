9e29eb0211f65cd730c7c7caba5d08c6a7fb6145
Marlyn <marlinp230@gmail.com>
2022-08-08 14:14:31 -0400

    make it better

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
-const mongoose=require("mongoose")
-
+const mongoose=require("mongoose") 
+const uri=`mongodb+srv://${process.env.DB}:${process.env.DB}@cluster0.dyxog.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`
 
   
   
-mongoose.connect(`${process.env.URI}`, {      
+mongoose.connect(uri, {      
  useNewUrlParser: true,    
  useUnifiedTopology: true, 
 
