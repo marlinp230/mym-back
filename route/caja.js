fcb568c12efb96c7faa51c6d74d2324665980681
Marlyn <marlinp230@gmail.com>
2022-08-09 12:53:28 -0400

    make it better

--- route/caja.js
+++ route/caja.js
@@ -6,16 +6,33 @@ const Caja = require('../models/caja')
 
 //Home
 router.get('/', async(req, res, next)=> {
- 
   try {
     const cajadb= await Caja.find()
     res.status(200).json(cajadb)
   } catch (error) {
     res.status(400).json(error)
   }
-   
-   
-  });
+});
 
+//add to db
+router.post('/', async function (req, res, next) {
+  const { Fecha,Nombre,Detalle,Monto } = req.body    
+
+
+ console.log(req.body)
+  try {
+    const orden= await Caja.find()
+    const caja = await new Caja({ Fecha, Detalle, Nombre,  Monto,orden:orden.length+1 });
+    console.log(orden.length)
+    await caja.save() 
+
+    res.json({caja,status:true,message:" Anadido en la Base de dato"})
+
+  } catch (error) {            
+    res.json({status:false,message:"NO pudo ser Anadido en la Base de dato"})
+
+  }
+
+});
 
 module.exports = router;
