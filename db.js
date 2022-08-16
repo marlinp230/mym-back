21d4df6221fdc35132e9659faa623456aa800257
Marlyn <marlinp230@gmail.com>
2022-08-16 17:21:30 -0400

    make it better

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose=require("mongoose") 
 const uri=`mongodb+srv://${process.env.DB}:${process.env.DB}@cluster0.dyxog.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`
-
-  
-    
-mongoose.connect(process.env.URI)
+ 
+   
+     console.log(process.env.URI)
+mongoose.connect(`${process.env.URI}`)
 .then(()=> console.log('db connected'))
 .catch(e=>console.log(e))       
 
