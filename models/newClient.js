fd068d896dbf6628d23d5125d4aeec146e16f2d8
Marlyn <marlinp230@gmail.com>
2022-09-02 13:16:37 -0400

    make it better

--- models/newClient.js
+++ models/newClient.js
@@ -8,11 +8,6 @@ const ClientSchema= new Schema({
     orden: {
         type: Number,
         required: [true, "Necesito numero de orden"]
-    },
-    Telefono:{
-        type:Number,
-        required: [true, "Necesito numero de telefono"],
-        unique:[true,"El numero de telefono ya existe"]
     }
     
      
