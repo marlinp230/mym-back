11e31c02f64b36543a884c7afc0b4231f5b6c8e9
marlinp230@gmail.com <marlinp230@gmail.com>
2023-03-04 10:43:10 -0800

    setdb

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const mongoose=require("mongoose") 
 
- 
-   
+  
+  
 mongoose.connect(`${process.env.URI}`)
 .then(()=> console.log('db connected'))
-.catch(e=>console.log(e))       
+.catch(e=>console.log(e))        
 
       
 // git add .
 // git commit -am "make it better"
-//  git push heroku main          
+//  git push heroku main            
